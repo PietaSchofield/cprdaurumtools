65df74ce9d4dc0d68eab16e557c72f97c707f4eb
Pieta Schofield <pieta.schofield@me.com>
2023-08-26 16:10:10 +0100

    pp

---
+++ R/load_probs.R
@@ -0,0 +1,47 @@
+# get observations batch
+#'
+#' get the drugissue records and convert some fields to useful field types
+#'
+#' @param bdir the name of the batch subdirectory
+#' @param odir the name of the output directory
+#' @param olist the list of observation codes
+#' @param bpp BiocParallal Multicore Parameters
+#'
+#'
+#' Pass a table of covariate codes and generate covariates table
+#' @import magrittr
+#' @export
+load_probs <- function(pddir,dbf,ow=F,db=F,tab_name="problems",
+    selvars=c("patid","obsid","parentprobobsid","probenddate","expduration","lastrevdate",
+              "parentprobrelid","probstatusid")){
+  obsfiles <- list.files(pddir,pattern="Prob.*txt$",full=T,recur=T)
+  dbi <- RSQLite::dbConnect(RSQLite::SQLite(),dbf)
+  nrec <- 0
+  if(!tab_name%in%RSQLite::dbListTables(dbi) || ow){
+    if(tab_name%in%RSQLite::dbListTables(dbi)){
+      RSQLite::dbExecute(dbi,paste0("DROP TABLE ",tab_name,";"))
+    }
+    nrec <- lapply(obsfiles,function(fn){
+      dat <- readr::read_tsv(fn,col_types=readr::cols(.default=readr::col_character())) %>%
+        dplyr::select(dplyr::all_of(selvars)) %>%
+        dplyr::mutate(probeddate = format(lubridate::dmy(probenddate)),
+                      lastrevdate = format(lubridate::dmy(lastrevdate)))
+      if(tab_name%in%RSQLite::dbListTables(dbi)){
+        app=T
+        ovr=F
+      }else{
+        app=F
+        ovr=T
+      }
+      RSQLite::dbWriteTable(dbi,tab_name,dat,overwrite=ovr,append=app)
+      nr <- dat %>% nrow()
+      cat(paste0(basename(fn),": ",nr," records loaded\n"))
+      rm(dat)
+      gc()
+      return(nr)
+    })
+  }
+  RSQLite::dbDisconnect(dbi)
+  trec <- sum(unlist(nrec))
+  return(cat(paste0(trec," records processed\n")))
+}

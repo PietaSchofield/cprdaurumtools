6cdb37c8175a44fe24d1d7850bdcadf19ebf1163
Pieta Schofield <pieta.schofield@me.com>
2024-05-22 14:43:24 +0100

    Revert "adding some convenience functions"
    This reverts commit 877f193a96e3036fdb27cc7b2294485e90012f19.

--- R/list_tables.R
+++
@@ -1,9 +0,0 @@
-#' List tables in the database
-#'
-#' @export
-list_tables <- function(dbf){
-  dbc <- duckdb::dbConnect(duckdb::duckdb(),dbf)
-  ret <- duckdb::dbListTables(dbc)
-  duckdb::dbDisconnect(dbc,shutdown=T)
-  ret
-}

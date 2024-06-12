79651daa5f9e02a6d0ed312a637e71c52d620a67
Pieta Schofield <pieta.schofield@me.com>
2024-06-12 17:46:52 +0100

    pp

--- R/gen_drugearas_table.R
+++ R/gen_drugearas_table.R
@@ -4,32 +4,101 @@
 #' @export
 gen_drugeras_table <- function(dbf,newtab,tabname,substancefield,db=F){
   if(db){
-    tabname <- "exposure_codes"
-    newtab <- "drug_exposures"
-    substancefield <- "substancelink"
+    tabname <- "drug_codes"
+    newtab <- "drug_eras"
+    dosetab <- "dose_codes"
+    substancefield="atc"
     dbf <- dbif
   }
 
-  dec_sql <- str_c("
+  make_ranked_issues_sql <- stringr::str_c("
+  CREATE TABLE
+    issue_grouping
+  AS (
+  WITH ranked_issues AS (
     SELECT DISTINCT
-      d.*,
-      e.",substancefield," AS substance
+      d.patid,
+      d.prodcodeid,
+      d.dosageid,
+      CAST(d.issuedate AS DATE) issuedate,
+      ROW_NUMBER() OVER (PARTITION BY d.patid, d.prodcodeid, d.dosageid ORDER BY issuedate) AS rn
     FROM 
       drug_issues AS d
     INNER JOIN
       ",tabname," AS e
-      ON e.prodcodeid=d.prodcodeid")
+      ON e.prodcodeid=d.prodcodeid
+      ),
+  date_differences AS (
+    SELECT
+      patid,
+      prodcodeid,
+      dosageid,
+      issuedate,
+      rn,
+      LAG(issuedate,1) OVER (PARTITION BY patid, prodcodeid, dosageid ORDER BY issuedate) AS previssue
+    FROM
+      ranked_issues)
+  SELECT 
+    patid,
+    prodcodeid,
+    dosageid,
+    issuedate,
+    rn,
+    previssue,
+    CASE 
+      WHEN previssue IS NULL THEN 0
+      ELSE DATE_DIFF('MONTH',previssue,issuedate)
+    END AS monthdiff,
+    SUM(CASE WHEN DATE_DIFF('MONTH',previssue,issuedate) >3 THEN 1 ELSE 0 END) OVER
+        (PARTITION BY patid, prodcodeid, dosageid ORDER BY issuedate) AS period_group
+  FROM 
+    date_differences)")
+
 
   dbi <- duckdb::dbConnect(duckdb::duckdb(),dbf)
-  dbListTables(dbi)
-  if(!newtab %in% dbListTables(dbi)){
-    val <- dbGetQuery(dbi,dec_sql)
-    ext <- "new records"
-  }else{
-    val <- dbGetQuery(dbi, paste0("SELECT COUNT(*) FROM ",newtab))
-    ext <- "records exist"
-  }
+  if("issue_grouping"%in%dbListTables(dbi)) dbExecute(dbi,"DROP TABLE issue_grouping")
+  dbExecute(dbi,make_ranked_issues_sql)
   dbDisconnect(dbi)
-  return(paste(val,ext))
+
+  group_sql <- stringr::str_c("
+  CREATE TABLE
+    ",newtab,"
+  AS(
+  SELECT
+    ig.patid,
+    dc.atc,
+    dc.moiety,
+    '' AS daily_dose,
+    MIN(ig.issuedate) AS start_date,
+    MAX(ig.issuedate) AS end_date,
+    COUNT(*) AS months
+  FROM
+    issue_grouping AS ig
+  INNER JOIN
+    ",tabname," AS dc
+    ON dc.prodcodeid=ig.prodcodeid
+  INNER JOIN
+    ",dosetab," AS dd
+    ON dd.dosageid=ig.dosageid
+  GROUP BY
+    ig.patid,
+    dc.atc,
+    dc.moiety,
+    dc.termfromemis,
+    dd.dosage_text,
+    dd.daily_dose,
+    dd.dose_unit,
+    period_group
+  HAVING 
+    COUNT(*) >= 1) ")
+
+  dbi <- duckdb::dbConnect(duckdb::duckdb(),dbf)
+  if(newtab %in% dbListTables(dbi)) dbExecute(dbi,str_c("DROP TABLE ",newtab," ;"))
+  dbExecute(dbi,group_sql) 
+  dbDisconnect(dbi)
+
 }
+
+
+
 
